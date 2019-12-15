e238ef29fa6b3895fc3104b7843b5ad3601c80d5
wanken <jokerwaver@gmail.com>
2019-12-15 08:57:02 +0800

    创建项目

---
+++ src/views/DialogManger.vue
@@ -0,0 +1,22 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'DialogManger',
+  data(){
+    return{}
+  },
+  mounted () {
+    this.$axios.get("")
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,21 @@
+<template>
+<el-container>
+  <el-row>
+    <el-col>
+      <el-button type="danger">
+        Click Me
+      </el-button>
+    </el-col>
+  </el-row>
+</el-container>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+
+<style scoped>
+
+</style>
